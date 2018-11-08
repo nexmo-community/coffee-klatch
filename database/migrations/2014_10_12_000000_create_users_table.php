de532d835bbc174e343197263fc5c28c821c32f5
Steve Crow <steve.crow@vonage.com>
2018-11-08 14:26:31 -0500

    Update user table for room and google auth integration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,9 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->string('avatar_original')->nullable();
+            $table->integer('room_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
